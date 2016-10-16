9c927af38f7d0b2a927c7ad8a6f9e543b137a597
Troy <tward4@tulane.edu>
2016-10-16 16:37:35 -0400

    Create databases

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import org.h2.tools.Server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Server.createWebServer().start();
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        createTables(conn);
+    }
 
+    public static void createTables (Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS doctors (id IDENTITY,name VARCHAR,specialty" +
+                "VARCHAR,address VARCHAR,cost INT,user_id INT)");
+        stmt.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY,name VARCHAR,password VARCHAR)");
     }
 }
